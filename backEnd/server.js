ae424afcbeeb608149f2c65179dc10671ede3a3b
Anthony Bernard Cereno <cereno.abc@gmail.com>
2023-03-12 22:37:12 +0800

    finished signup and signin validation

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,5 +1,7 @@
+// initialization
 const express = require('express');
 const fs = require('fs');
+const path = require("path");
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
@@ -7,54 +9,68 @@ const postsData = require("./data/posts.json");
 const companyData = require("./data/companyData.json");
 const userData = require("./data/userData.json");
 
+// initializing path and URL
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'frontEnd/majorproject-app/build')));
 
-app.set("view engine", "ejs");
-
-//-- home
-app.get("/", (req, res) => {
-    res.render('index');
-});
-//-- about
-app.get("/about", (req, res) => {
-    res.render('about');
-});
-//-- contact
-app.get("/contact", (req, res) => {
-    res.render('contact');
+// home page
+app.get("/", (req, res)=>{
+  res.sendFile(__dirname + "/public/pages/index.html");
 });
 
-// Handle POST requests to /submit-form
-app.post('/submit-form-user', (req, res) => {
+// Endpoints - form for user
+app.post('/posts', (req, res) => {
   const formData = req.body;
-  // userData.push(formData);
   postsData.unshift({
     ...formData,
     id:('100' + (postsData.length + 1).toString())
-});
-  fs.writeFileSync('./data/userData.json', JSON.stringify(userData));
-  console.log(userData); 
+});;
+  fs.writeFileSync('./data/posts.json', JSON.stringify(postsData));
+  console.log(userData);
+
   // Send a response to the client
   res.send('Form submitted successfully!');
 });
+app.post('/submit-form-user', (req, res) => {
+  const formData = req.body;
+  userData.unshift({
+    ...formData,
+    id:('100' + (userData.length + 1).toString())
+});;
+  fs.writeFileSync('./data/userData.json', JSON.stringify(userData));
+  console.log(userData);
+
+  // Send a response to the client
+  res.send('Form submitted successfully!');
+});
+// Endpoints - form for company
 app.post('/submit-form-company', (req, res) => {
   const formData = req.body;
-  companyData.push(formData);// Write the form data to the companyData.json file
+  companyData.unshift({
+    ...formData,
+    id:('100' + (companyData.length + 1).toString())
+});;
   fs.writeFileSync('./data/companyData.json', JSON.stringify(companyData));
-  console.log(companyData); // Add this line to check if the server is correctly writing the form data to your posts.json file
+  console.log(companyData);
 
   // Send a response to the client
   res.send('Form submitted successfully!');
 });
-app.post('/submit-form', (req, res) => {
-  const formData = req.body;
-  postsData.push(formData);// Write the form data to the posts.json file
-  fs.writeFileSync('./data/posts.json', JSON.stringify(postsData));
-  console.log(postsData); // Add this line to check if the server is correctly writing the form data to your posts.json file
 
-  // Send a response to the client
-  res.send('Form submitted successfully!');
+app.get('/posts', (req, res) => {
+  const posts = fs.readFileSync('./data/posts.json');
+  res.send(JSON.parse(posts));
+});
+app.get('/userData', (req, res) => {
+  const userData = fs.readFileSync('./data/userData.json');
+  res.send(JSON.parse(userData));
+});
+
+app.get('/companyData', (req, res) => {
+  const companyData = fs.readFileSync('./data/companyData.json');
+  res.send(JSON.parse(companyData));
 });
 
 // Start the server and listen on the specified port
